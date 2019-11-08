5b1db8796bb72c1c2dc367df2f8c9c44a2399726
Oscar Sanchez <oscarluis.sanchez@gmail.com>
2019-11-07 20:49:13 -0600

    feat(schema): Implements schema

--- src/handlers/handler.ts
+++ src/handlers/handler.ts
@@ -1,3 +1,18 @@
+const { graphqlLambda, graphiqlLambda } = require('apollo-server-lambda');
+import { makeExecutableSchema } from 'graphql-tools';
+import schema from './src/grahql/schema.graphql';
+import { resolvers } from './resolvers';
+import { GraphQLSchema } from 'graphql';
+import {
+  APIGatewayProxyEvent,
+  Context,
+  APIGatewayProxyHandler,
+} from 'aws-lambda';
+const myGraphQLSchema: GraphQLSchema = makeExecutableSchema({
+  typeDefs: schema,
+  resolvers,
+});
+
 module.exports.hello = async (event: object): Promise<object> => {
   return {
     statusCode: 200,
@@ -12,4 +27,19 @@ module.exports.hello = async (event: object): Promise<object> => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+exports.graphqlHandler = function graphqlHandler(
+  event: APIGatewayProxyEvent,
+  context: Context,
+  callback: Function
+): APIGatewayProxyHandler {
+  function callbackWithHeaders(error, output) {
+    // eslint-disable-next-line no-param-reassign
+    output.headers['Access-Control-Allow-Origin'] = '*';
+    callback(error, output);
+  }
+
+  const handler = graphqlLambda({ schema: myGraphQLSchema });
+  return handler(event, context, callbackWithHeaders);
 };
